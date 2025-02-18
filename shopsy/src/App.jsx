dd4d9e67a52278a46f7d54e7399452e6cdb64b59
Devesh Chandra <yashdev2712@gmail.com>
2025-02-19 04:49:53 +0530

    added aos for products and hero section

--- shopsy/src/App.jsx
+++ shopsy/src/App.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
+import Products from "./components/Products/Products";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 const App = () => {
+  useEffect(() => {
+    Aos.init({
+      offset: 100,
+      duration: 100,
+      easing: "ease-in-out-sine",
+      delay: 100,
+    });
+    Aos.refresh();
+  }, []);
+
   return (
     <div className="">
       <Navbar />
       <Hero />
+      <Products />
     </div>
   );
 };
